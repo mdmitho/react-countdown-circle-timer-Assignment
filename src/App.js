aee2cc8e40272ffc33f34c645381f92a70586411
Hossen <mdmitho199911@gmail.com>
2022-07-16 23:03:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Time from './Components/Taimar/Time';
+
+function App() {
+  return (
+    <div className="App">
+     <Time></Time>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Taimar/Time.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Time.css"
+
+const Time = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Time;
